14eb2f41de4a2ddbe9ff65b72e7446d74b44cdcf
Pablo Alberto Barocio <pbarocio@github.com>
2026-07-10 22:35:04 -0600

    Se terminó el Scrip, se logró hacer en una sola sesión SSH hacer los 4 pasos

--- Respaldo_Routers_Python/bkcps_handler.py
+++ Respaldo_Routers_Python/bkcps_handler.py
@@ -1,16 +1,20 @@
 import logging
 import datetime #Importar librería para manejar fecha y hora
+import time
 from pathlib import Path #Librería para manejar directorios cómo objetos
 import subprocess #Librería para comandos de shell
 from topology import load_branches
 from ssh_manager import connect_router
 from alert_manager import send_notification
+from netmiko import FileTransfer
+import paramiko
 
-def get_src(branch_name,branch_ip,branch_dir,command_export,config,time_format):
+def get_src(branch_name,branch_dir,config,time_format,ssh_session):
     try:
         file_bckp_name = f"{branch_name}_{time_format}.rsc" #Definimos el nombre de los archivos rsc
         write_file = branch_dir / file_bckp_name #Ruta para escribir el resultado del comando /export
-        data_export = connect_router(branch_name,branch_ip,command_export,config) #Enviamos el comando para su ejecución en el Router
+        command_export = "/export" #Comando de Mikrotik que nos extrae toda la configuración que se ejecuta actualmente
+        data_export = ssh_session.send_command(command_export) #Enviamos el comando para su ejecución en el Router
         if data_export is not None:
             write_file.write_text(data_export) #Escribir con Path
             logging.info(f"✅ \"{file_bckp_name}\" GENERADO CORRECTAMENTE...")
@@ -21,47 +25,77 @@ def get_src(branch_name,branch_ip,branch_dir,command_export,config,time_format):
         logging.critical(f"❌ ERROR AL ESCRIBIR COMANDO EN {write_file} \n {error}", exc_info=True)
         send_notification(config,f"❌ ERROR AL GUARDAD COMANDO EN {write_file} \nREVISA EL SERVIDOR \n{error}")
 
-def get_backup(branch_name,branch_ip,command_backup,router_file,config):
+def get_backup(branch_name, branch_dir, time_format, config, ssh_session):
     try:
-        data_backup = connect_router(branch_name,branch_ip,command_backup,config)
+        router_file = f"{branch_name}_{time_format}.backup" #Definir el nombre que va a tener el Binario en el router qué se descargará al equipo
+        command_backup = f"/system backup save name={router_file} dont-encrypt=yes" #Comando de Mikrotik para que nos genere el respaldo binario
+        local_file = branch_dir / router_file #Establecer ruta de binarios resultantes
+        data_backup = ssh_session.send_command(command_backup)
         if "backup saved" in data_backup:
             logging.info(f"✅ \"{router_file}\" GENERADO CORRECTAMENTE...")
         else:
-            logging.info(f"❌ ¡¡¡ERROR!!! {branch_name} NO PUDO GENERAR EL BINARIO: \"{router_file}\"...\n\"{data_backup}\"")
-            send_notification(config,f"❌¡¡ERROR!! {branch_name} NO PUDO GENERAR EL BINARIO: \n\"{router_file}\"...\n\"{data_backup}\"")
-            raise RuntimeError(f"Fallo al generar backup en el router: {router_file}")
+            logging.info(f"❌ ¡¡¡ERROR!!! {branch_name} NO PUDO GENERAR EL BINARIO: \"{router_file}\"... \"{data_backup}\"")
+            #send_notification(config,f"❌¡¡ERROR!! {branch_name} NO PUDO GENERAR EL BINARIO: \n\"{router_file}\"...\n\"{data_backup}\"")
+        return True, router_file, local_file
     except Exception as error:
         logging.error(f"❌ ERROR EJECUTANDO EL BACKUP {router_file} \n {error}")
-        send_notification(config,f"❌ ¡¡¡ERROR GENERANDO RESPALDO EN :{branch_name} \n NO SE PUDO EJECUTAR EL COMANDO \n{router_file}!")
+        #send_notification(config,f"❌ ¡¡¡ERROR GENERANDO RESPALDO EN :{branch_name} \n NO SE PUDO EJECUTAR EL COMANDO \n{router_file}!")
+        return False, router_file, local_file
 
-def scp_connect(branch_ip,router_file,local_file,config):
+def scp_connect_native(router_file, local_file, config, ssh_session):
     try:
-        ssh_port = config["port"]
-        key_path = config["key_path"]
-        user_router = config["username"]
-        scp_command = [
-            "scp",
-            "-P", str(ssh_port),
-            "-i", key_path,
-            f"{user_router}@{branch_ip}:{router_file}",
-            str(local_file),
-        ]
-        subprocess.run(scp_command, check=True, capture_output=True, text=True)
-        #scp_command = f"scp -P {ssh_port} -i {key_path} {user_router}@{branch_ip}:{router_file} {local_file}" # Construir el comando scp
-        #subprocess.run(scp_command, shell=True, check=True, capture_output=True, text=True) #Ejecutar el comando SCP para copiar el archivo del router
-        logging.info(f"✅ \"{router_file}\" DESCARGADO CORRECTAMENTE...")
-    except subprocess.CalledProcessError as e:
-        logging.error(f"❌ SCP falló: {e.stderr}")
-        send_notification(config,f"❌ ERROR AL COPIAR {router_file} EN EL SERVIDOR \nREVISA QUÉ ESTÉ ENCENDIDO")
+        # ssh_port = config["port"]
+        # key_path = config["key_path"]
+        # user_router = config["username"]
+        # scp_command = [
+        #     "scp",
+        #     "-P", str(ssh_port),
+        #     "-i", key_path,
+        #     f"{user_router}@{branch_ip}:{router_file}",
+        #     str(local_file),
+        # ]
+        # subprocess.run(scp_command, check=True, capture_output=True, text=True)
+        # scp_command = f"scp -P {ssh_port} -i {key_path} {user_router}@{branch_ip}:{router_file} {local_file}" # Construir el comando scp
+        # subprocess.run(scp_command, shell=True, check=True, capture_output=True, text=True) #Ejecutar el comando SCP para copiar el archivo del router
+        #paramiko_client = ssh_session.remote_conn
+        
+        # transport = ssh_session.remote_conn.transport
+        # sftp = transport.open_sftp()
+        
+        # sftp = ssh_session.remote_conn.open_sftp()
+        # sftp.get(router_file, str(local_file))
+        # sftp.close()
+        # with FileTransfer(ssh_conn=ssh_session, source_file=router_file, dest_file=local_file, direction='get') as scp_transfer:
+        #     if scp_transfer.check_file_exist():
+        #         logging.info("📍 El archivo existe en el MikroTik. Verificando tamaño...")
+        #         logging.info(f"✅ \"{router_file}\" DESCARGADO CORRECTAMENTE...")
+        
+        transport = ssh_session.remote_conn.transport
+        # 2. Crear un cliente SFTP directamente desde el Transport
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        # 3. Descargar el archivo
+        sftp.get(router_file, str(local_file))
+        # 4. Cerrar el canal SFTP
+        sftp.close()
+        logging.info(f"✅ \"{local_file}\" DESCARGADO CORRECTAMENTE...")
+        return True
+    # except subprocess.CalledProcessError as e:
+    #     logging.error(f"❌ SCP falló: {e.stderr}")
+    #     return False
+    except Exception as e:
+        logging.error(f"❌ ERROR AL COPIAR {router_file} EN EL SERVIDOR REVISA QUÉ ESTÉ ENCENDIDO\n{e}", exc_info=True)
+        return False
+        #send_notification(config,f"❌ ERROR AL COPIAR {router_file} EN EL SERVIDOR \nREVISA QUÉ ESTÉ ENCENDIDO")
 
-def remove_backup_file(branch_name,branch_ip,remove_command,router_file,config):
+def remove_backup_file(router_file, config, ssh_session):
     try:
-        data_remove = connect_router(branch_name,branch_ip,remove_command,config) #Enviamos el comando de eliminar al router
+        remove_command = f"/file remove [find name={router_file}]" #Construir el comando para eliminar el archivo generado en el Router
+        data_remove = ssh_session.send_command(remove_command) #Enviamos el comando de eliminar al router
         if "no such item" in data_remove.lower() or "failure" in data_remove.lower():
-            logging.info(f"\n *** ⚠️ ATENCIÓN: NO SE PUDO ELIMINAR {router_file} DEL ROUTER. REVISAR MANUALMENTE... CERRANDO CONEXIÓN...")
+            logging.info(f"\n *** ⚠️ ATENCIÓN: NO SE PUDO ELIMINAR {router_file} DEL ROUTER. REVISAR MANUALMENTE... \n CERRANDO CONEXIÓN...  \n")
             send_notification(config,f"⚠️ ATENCIÓN: NO SE PUDO ELIMINAR {router_file} DEL ROUTER. \nREVISAR MANUALMENTE... \n")
         else:
-            logging.info(f"\n🧹 \"{router_file}\" ELMINADO CORRECTAMENTE DEL ROUTER...\n")
+            logging.info(f"🧹 \"{router_file}\" ELMINADO CORRECTAMENTE DEL ROUTER...")
     except Exception as error:
         logging.error(f"❌ ERROR ELIMINANDO {router_file} \n {error}")
 
@@ -72,19 +106,26 @@ def orchestration(config,bckps_dir):
         branches = load_branches(config)
         logging.info("=" * 100 + "\n\t\t\t\t\t\tGENERANDO RESPALDOS DE ROUTERS AGROCISA\n" + "=" * 130)
         for branch_name, branch_ip in branches.items():
-            branch_dir = bckps_dir / str(branch_name) # Definir la ruta del Directorio de la Sucursal
-            branch_dir.mkdir(parents=True, exist_ok=True) # Crear directorios si no existen
-            logging.info("*" * 70 + "\n" + f"*** 📡 INTENTANDO CONEXIÓN A {branch_name.upper()} ({branch_ip}) .... " + "\n" + "*" * 100)
-            command_export = "/export" #Comando de Mikrotik que nos extrae toda la configuración que se ejecuta actualmente
-            get_src(branch_name,branch_ip,branch_dir,command_export,config,time_format) #Correr el backup de rsc
-            router_file = f"{branch_name}_{time_format}.backup" #Definir el nombre que va a tener el Binario en el router qué se descargará al equipo
-            command_backup = f"/system backup save name={router_file} dont-encrypt=yes" #Comando de Mikrotik para que nos genere el respaldo binario
-            local_file = branch_dir / router_file #Establecer ruta de binarios resultantes
-            get_backup(branch_name,branch_ip,command_backup,router_file,config)
-            scp_connect(branch_ip,router_file,local_file,config) #Descargamos el backup del router por scp al directorio de destino
-            remove_command = f"/file remove [find name={router_file}]" #Construir el comando para eliminar el archivo generado en el Router
-            remove_backup_file(branch_name,branch_ip,remove_command,router_file,config)
+            try:
+                branch_dir = bckps_dir / str(branch_name) # Definir la ruta del Directorio de la Sucursal
+                branch_dir.mkdir(parents=True, exist_ok=True) # Crear directorios si no existen
+                logging.info("*" * 70) 
+                logging.info(f"*** 📡 INTENTANDO CONEXIÓN A {branch_name.upper()} ({branch_ip}) .... ")
+                logging.info("*" * 100)
+                ssh_session = connect_router(branch_name, branch_ip, config)
+                get_src(branch_name, branch_dir, config, time_format, ssh_session) #Correr el backup de rsc
+                success, router_file, local_file = get_backup(branch_name,branch_dir,time_format,config,ssh_session)
+                if success: #Sí la ejecución del backup se hace con éxito
+                    success_scp = scp_connect_native(router_file, local_file, config, ssh_session) #Descargamos el backup del router por scp al directorio de destino
+                    #remove_backup_file(router_file,config,ssh_session) #Eliminamos el archivo del router
+                remove_backup_file(router_file,config,ssh_session)
+            except Exception as error:
+                logging.error(f"❌ Flujo interrumpido en {branch_name} debido a un fallo: {error}", exc_info=True)
+            finally:
+                    ssh_session.disconnect()
+                    logging.info(f"*** SE CERRÓ LA SESIÓN SSH CON \"{branch_name.upper()}\" 🔓...")
             
-        #send_notification(config,f"💾 LOS RESPALDOS DE LOS ROUTERS FUERON GENERADOS CON ÉXITO... ✅")
+        send_notification(config,f"💾 LOS RESPALDOS DE LOS ROUTERS FUERON GENERADOS CON ÉXITO... ✅")
     except Exception as error:
+        logging.info(f"❌ ERORR AL GENERAR LOS RESPALDOS DE LOS ROUTERS... \n{error}", exc_info=True)
         send_notification(config,f"❌ ERORR AL GENERAR LOS RESPALDOS DE LOS ROUTERS... \n{error}")
--- Respaldo_Routers_Python/ssh_manager.py
+++ Respaldo_Routers_Python/ssh_manager.py
@@ -4,7 +4,7 @@ from netmiko import ConnectHandler #Librería para manejar conexión SSH al rout
 from alert_manager import send_notification
 import logging
 
-def connect_router(branch_name,branch_ip,command,config):
+def connect_router(branch_name,branch_ip,config):
     connection_data = { # Definimos los parámetros de la conexión
         'device_type': config["device_type"],
         'host': branch_ip,
@@ -17,19 +17,20 @@ def connect_router(branch_name,branch_ip,command,config):
     }
     
     try:
-        connection = ConnectHandler(**connection_data) # Abrimos el puente SSH
-        data = connection.send_command(command) ##Eviar comando al router
-        connection.disconnect()
+        ssh_session = ConnectHandler(**connection_data) # Abrimos el puente SSH
         
-        return data
+        return ssh_session
     except NetmikoTimeoutException: # El Mikrotik no responde (enlace muerto, sin luz, IP mal)
         logging.warning(f"❌ ¡¡¡TIMEOUT EN {branch_name}! NO HAY RESPUESTA DEL ROUTER...")
         send_notification(config,f"🛑 {branch_name} ESTÁ INACCESIBLE (Timeout).")
+        return []
     
     except NetmikoAuthenticationException: # Alguien le movió al usuario o contraseña del Mikrotik
         logging.error(f"🔐 ¡¡¡ERROR DE AUTENTICACIÓN EN {branch_name}!!! CREDENCIALES INCORRECTAS.,.")
         send_notification(config,f"⚠️ ALERTA DE SEGURIDAD:\nCREDENCIALES RECHAZADAS EN {branch_name}.")
+        return []
     
     except (SSHException, Exception) as e: # Un error raro del protocolo SSH o cualquier otra falla inesperada
         logging.critical(f"💥 ¡¡¡ERROR INESPERADO EN {branch_name}: {str(e)}")
         send_notification(config,f"🚨 ¡¡¡FALLA CRÍTICA SSH EN {branch_name}: \n{str(e)}")
+        return []
